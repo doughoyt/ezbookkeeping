77557857057b0915c8ff85002dac6ec5d6748b4f
Doug Hoyt <doughoyt@gmail.com>
2025-04-16 20:07:59 -0600

    Fix duplicate toggle methods; fix message return

--- src/views/desktop/transactions/ListPage.vue
+++ src/views/desktop/transactions/ListPage.vue
@@ -1407,29 +1407,8 @@ function show(transaction: Transaction): void {
 function toggle(transaction: Transaction): void {
     transactionsStore.toggleClearedTransaction({
             transaction: transaction as Transaction
-    }).then(result => {
-        if (result && result.message) {
-            snackbar.value?.showMessage(result.message);
-        }
-
-        reload(false);
-    }).catch(error => {
-        if (error) {
-            snackbar.value?.showError(error);
-        }
-    });
-
-}
-
-function toggle(transaction: Transaction): void {
-    transactionsStore.toggleClearedTransaction({
-            transaction: transaction as Transaction
-    }).then(result => {
-        if (result && result.message) {
-            snackbar.value?.showMessage(result.message);
-        }
-
-        reload(false);
+    }).then(() => {
+        reload(false, false);
     }).catch(error => {
         if (error) {
             snackbar.value?.showError(error);
